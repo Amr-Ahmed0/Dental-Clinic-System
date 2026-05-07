a59c9909d138d5b4266f941128bec10fc543d8db
Amr Ahmed Sayed <amrahmedsayde@gmail.com>
2026-05-07 22:40:21 +0300

    fix

--- server.js
+++ server.js
@@ -574,5 +574,7 @@ app.post('/api/payments', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+// const PORT = process.env.PORT || 5001;
+// app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+
+export default app;
